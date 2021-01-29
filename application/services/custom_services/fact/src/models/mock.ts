495ddadfd49447debd2e66cdc88b4296bee7dbb8
immanikandan <immanikandan>
2021-01-29 06:39:58 +0000

    updated generated code

--- application/services/custom_services/fact/src/models/mock.ts
+++ application/services/custom_services/fact/src/models/mock.ts
@@ -1,13 +1,7 @@
-
-import * as mongoose from 'mongoose';
-
-const Schema = mongoose.Schema;
-
-export const mockSchema = new Schema({
+var mockModel = {
    name: String,
    place: String,
    phone: String
-})
+};
 
-const mockModel = mongoose.model('mock', mockSchema, 'mock');
-export default mockModel;
+module.exports = mockModel;
